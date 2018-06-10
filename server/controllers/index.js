f19d6bd7da5b122d691520fc8aee368be675503e
Nikita Biryukov <biryukov.nikita96@mail.ru>
2018-06-11 00:37:20 +0300

    Реализовано апи обновления туду листа у пользователя.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,9 +10,23 @@ module.exports.login = (req, res) => {
 };
 
 module.exports.registration = (req, res) => {
-    
+
 };
 
 module.exports.update = (req, res) => {
-
+    var todos = req.body.todos;
+    var login = req.body.login;
+    User.updateOne({ login: login }, { todos: todos }, (err, doc) => {
+        if (err) {
+            console.log(err);
+            res.json({
+                ok: false
+            });
+        } else if (doc.nModified != 0) {
+            console.log(doc);
+            res.json({
+                ok: true
+            });
+        }
+    });
 };
